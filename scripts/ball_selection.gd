ebde89fad82bf35934ab377cc1d0112055dbc9f9
Zane Helton <zane99@me.com>
2022-08-14 10:02:33 -0400

    Add ball deselection

--- scripts/ball_selection.gd
+++ scripts/ball_selection.gd
@@ -1,6 +1,15 @@
 extends Node
 
+# Store the circles that are selected
+var selected_circles = []
+
 func _handle_circle_pressed(circle: CircleNumber):
 	var mat = circle.get_node("Sprite").material as ShaderMaterial
+		
 	if (mat):
-		mat.set_shader_param("width", 24)
+		if (selected_circles.has(circle)):
+			mat.set_shader_param("width", 0)
+			selected_circles.erase(circle)
+		else:
+			mat.set_shader_param("width", 24)
+			selected_circles.append(circle)
